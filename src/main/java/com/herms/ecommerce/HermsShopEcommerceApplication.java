daa1fe80a8eb912387f859319cd3fef588d15bec
Emerson Ribeiro <emersonrjr03@gmail.com>
2023-09-03 12:31:55 +0100

    adding first files and application.properties

---
+++ src/main/java/com/herms/ecommerce/HermsShopEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.herms.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermsShopEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermsShopEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herms/ecommerce/HermsShopEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herms.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermsShopEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
